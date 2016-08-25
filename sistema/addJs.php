27eaf135b5499274385e4980e9e4507cb14b0163
LGerMa <lgmm.93@gmail.com>
2016-08-25 15:33:41 -0600

    se ha modificado usuarios.php ocupando dataTable

--- sistema/addJs.php
+++ sistema/addJs.php
@@ -10,3 +10,5 @@
     <!-- Custom Theme JavaScript -->
     <script src="../dist/js/sb-admin-2.js"></script>
     <script src="../js/main.js"></script>
+
+    <script type="text/javascript" language="javascript" src="../js/jquery.dataTables.js"></script>

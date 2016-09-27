39422406dc33991ed33a042df3d1fb87de1d54d5
Unknown <gena_albert@hotmail.com>
2016-09-27 14:34:16 -0600

    Menu

--- sistema/addJs.php
+++ sistema/addJs.php
@@ -1,5 +1,5 @@
  <!-- jQuery -->
-	<script type="text/javascript" src="../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -12,7 +12,4 @@
     <script src="../js/main.js"></script>
 
     <script type="text/javascript" language="javascript" src="../js/jquery.dataTables.js"></script>
-      <script type="text/javascript" src="../bower_components/moment/min/moment.min.js"></script>
-
-    <script type="text/javascript" src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+    

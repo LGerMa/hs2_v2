9a78910b277e95ec1bd53f13ff63743315ddc7bb
LGerMa <lgmm.93@gmail.com>
2016-11-19 16:33:10 -0600

    merge

--- php/cn.php
+++ php/cn.php
@@ -1,36 +1,5 @@
 <?php
 function cnx(){
-    $host="localhost";
-    $dbUser="root";
-    $dbPass="root";
-    $myDB="insafocoop";
-    $cnx=mysqli_connect($host,$dbUser,$dbPass,$myDB);
-    return $cnx;
-}
-
-function pruebaCnx(){
-    $host="localhost";
-    $dbUser="root";
-    $dbPass="root";
-    $myDB="insafocoop";
-    $cnx=mysqli_connect($host,$dbUser,$dbPass,$myDB);
-    $flag=0;
-    if (mysqli_connect_errno()) {
-        $flag=0;
-    }
-
-    if (mysqli_ping($cnx)) {
-        $flag=1;
-    } else {
-        $flag=0;
-    }
-    mysqli_close($cnx);
-    return $flag;
-}
-
-?>
-<?php
-/*function cnx(){
     $url = parse_url(getenv("mysql://b8f8f6119ca584:d8a28425@us-cdbr-iron-east-04.cleardb.net/heroku_6ac98248592533a?reconnect=true"));
     $host="us-cdbr-iron-east-04.cleardb.net";
     $dbUser="b8f8f6119ca584";
@@ -48,19 +17,25 @@ function pruebaCnx(){
     $myDB="heroku_6ac98248592533a";
     $cnx=mysqli_connect($host,$dbUser,$dbPass,$myDB);
     $flag=0;
+    /* comprobar la conexión */
     if (mysqli_connect_errno()) {
+        //printf("Conexi&oacute;n fallida: %s\n", mysqli_connect_error());
         $flag=0;
+       // exit();
     }
 
+    /* comprobar si el servidor sigue funcionando */
     if (mysqli_ping($cnx)) {
+        //printf ("¡La conexi&oacute;n est&aacute; bien!\n");
         $flag=1;
     } else {
+        //printf ("Error: %s\n", mysqli_error($enlace));
         $flag=0;
     }
 
-
+    /* cerrar la conexión */
     mysqli_close($cnx);
     return $flag;
 }
-*/
+
 ?>
